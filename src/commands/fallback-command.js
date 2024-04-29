9c5f144ecb0b354567b12fbe4510f24df0310267
Daniel Cruz <danielcunha54@gmail.com>
2024-04-29 10:44:10 -0300

    feat: append "run" to npm scripts

--- src/commands/fallback-command.js
+++ src/commands/fallback-command.js
@@ -1,7 +1,11 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
 import chalk from 'chalk'
 
 import { executeCommand } from '../execute-command.js'
 import { detectPackageManager } from '../utils/package-manager.js'
+import { getProjectRootDirectory } from '../utils/project.js'
 
 /**
  * Fallback command to forward the command to the detected package manager.
@@ -14,6 +18,37 @@ export async function fallbackCommand(program) {
     return console.error(chalk.red('Package manager not found!'))
   }
 
-  const command = `${packageManager} ${program.args.join(' ')}`
+  let managerCommand = program.args.join(' ').trim()
+
+  // Append "run" to the command if it's a script and the package manager is npm.
+  // The other managers don't require the "run" keyword.
+  if (packageManager === 'npm' && !managerCommand.startsWith('run')) {
+    const commandName = program.args[0]
+    const projectScripts = await loadProjectScripts()
+    const isScript = projectScripts.includes(commandName)
+
+    if (isScript) {
+      managerCommand = `run ${managerCommand}`
+    }
+  }
+
+  const command = `${packageManager} ${managerCommand}`
   executeCommand(command)
+}
+
+/**
+ * @returns {Promise<string[]>}
+ */
+async function loadProjectScripts() {
+  const rootDir = await getProjectRootDirectory({ ignoreLockFile: true })
+  if (rootDir) {
+    const packageJsonPath = path.join(rootDir, 'package.json')
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'))
+
+    if (packageJson.scripts) {
+      return Object.keys(packageJson.scripts)
+    }
+  }
+
+  return []
 }
